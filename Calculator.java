021b295c14dfac6505eb1321184b274d3898a761
Majid Ali <majidalishaikh13@gmail.com>
2026-07-26 15:50:12 +0500

    Updated Car and Calculator assignments

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,127 @@
-public class Calculator{
+// public class Calculator{
+//     private double num1;
+//     private double num2;
+
+//     public Calculator(double num1, double num2) {
+//         this.num1 = num1;
+//         this.num2 = num2;
+//     }
+//     public double add() {
+//         return num1 + num2;
+//     }
+//     public double subtract() {
+//         return num1 - num2;
+//     }
+//     public double multiply() {
+//         return num1 * num2;
+//     }
+//     public double divide() {
+//         if (num2 != 0) {
+//             return num1 / num2;
+//         } else {
+//             throw new ArithmeticException("Cannot divide by zero");
+//         }
+//     }
+//     public double modulus() {
+//         if (num2 != 0) {
+//             return num1 % num2;
+//         } else {
+//             throw new ArithmeticException("Cannot perform modulus by zero");
+//         }
+//     }
+//     public double power() {
+//             return Math.pow(num1, num2);
+//     }
+//     public double average() {
+//         return (num1 + num2) /2;
+//     }
+//     public double max() {
+//         if (num1 > num2){
+//             return num1;
+//         } else {
+//             return num2;
+//         }
+//     }
+
+//     public double addition(double...numbers){
+//         double sum = 0;
+//         for (double number : numbers) {
+//             sum += number;
+//         }
+//         return sum;
+//     }
+        
+//         public void showInfo() {
+//             System.out.println("Number 1: " + num1);
+//             System.out.println("Number 2: " + num2);
+//     }
+// }
+
+
+public class Calculator {
+
     private double num1;
     private double num2;
 
+    // Object Counter
+    private static int count = 0;
+
+    // Default Constructor
+    public Calculator() {
+        this.num1 = 0;
+        this.num2 = 0;
+        count++;
+    }
+
+    // Parameterized Constructor
     public Calculator(double num1, double num2) {
         this.num1 = num1;
         this.num2 = num2;
+        count++;
     }
+
+    // Copy Constructor
+    public Calculator(Calculator c) {
+        this.num1 = c.num1;
+        this.num2 = c.num2;
+        count++;
+    }
+
+    // Getters
+    public double getNum1() {
+        return num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    // Setters
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    // Object Count
+    public static int getObjectCount() {
+        return count;
+    }
+
     public double add() {
         return num1 + num2;
     }
+
     public double subtract() {
         return num1 - num2;
     }
+
     public double multiply() {
         return num1 * num2;
     }
+
     public double divide() {
         if (num2 != 0) {
             return num1 / num2;
@@ -22,6 +129,7 @@ public class Calculator{
             throw new ArithmeticException("Cannot divide by zero");
         }
     }
+
     public double modulus() {
         if (num2 != 0) {
             return num1 % num2;
@@ -29,30 +137,39 @@ public class Calculator{
             throw new ArithmeticException("Cannot perform modulus by zero");
         }
     }
+
     public double power() {
-            return Math.pow(num1, num2);
-    }
-    public double average() {
-        return (num1 + num2) /2;
-    }
-    public double max() {
-        if (num1 > num2){
-            return num1;
-        } else {
-            return num2;
-        }
+        return Math.pow(num1, num2);
     }
 
-    public double addition(double...numbers){
+    public double average() {
+        return (num1 + num2) / 2;
+    }
+
+    public double max() {
+        if (num1 > num2)
+            return num1;
+        else
+            return num2;
+    }
+
+    // Varargs
+    public double addition(double... numbers) {
         double sum = 0;
         for (double number : numbers) {
             sum += number;
         }
         return sum;
     }
-        
-        public void showInfo() {
-            System.out.println("Number 1: " + num1);
-            System.out.println("Number 2: " + num2);
+
+    public void showInfo() {
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+    }
+
+    @Override
+    public String toString() {
+        return "Number 1: " + num1 +
+               "\nNumber 2: " + num2;
     }
 }
--- MainCalculator.java
+++ MainCalculator.java
@@ -1,21 +1,104 @@
+// public class MainCalculator {
+//     public static void main(String[]args){
+//         Calculator myCalculator = new Calculator(10, 5);
+//         Calculator yourCalculator = new Calculator(20, 0);
+
+//         myCalculator.showInfo();
+//         System.out.println("Addition: " + myCalculator.add());
+//         System.out.println("Subtraction: " + myCalculator.subtract());
+
+//         yourCalculator.showInfo();
+//         try{
+//             System.out.println("Division: " + yourCalculator.divide());
+//         } catch (ArithmeticException e) {
+//             System.out.println("Error: " + e.getMessage());
+//         }
+
+//         System.out.println("Addition of multiple numbers: " + yourCalculator.addition(5,3,7,8,1,9));
+        
+
+//     }
+// }
+
+
+
+
 public class MainCalculator {
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
+
+        // Parameterized Constructor
         Calculator myCalculator = new Calculator(10, 5);
         Calculator yourCalculator = new Calculator(20, 0);
 
+        // Default Constructor
+        Calculator defaultCalculator = new Calculator();
+
+        // Copy Constructor
+        Calculator copyCalculator = new Calculator(myCalculator);
+
+        System.out.println("===== My Calculator =====");
         myCalculator.showInfo();
         System.out.println("Addition: " + myCalculator.add());
         System.out.println("Subtraction: " + myCalculator.subtract());
+        System.out.println("Multiplication: " + myCalculator.multiply());
+        System.out.println("Division: " + myCalculator.divide());
+        System.out.println("Power: " + myCalculator.power());
+        System.out.println("Average: " + myCalculator.average());
+        System.out.println("Maximum: " + myCalculator.max());
 
+        System.out.println();
+
+        System.out.println("===== Edge Case =====");
         yourCalculator.showInfo();
-        try{
+
+        try {
             System.out.println("Division: " + yourCalculator.divide());
         } catch (ArithmeticException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-        System.out.println("Addition of multiple numbers: " + yourCalculator.addition(5,3,7,8,1,9));
-        
+        try {
+            System.out.println("Modulus: " + yourCalculator.modulus());
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
+        System.out.println();
+
+        System.out.println("Varargs Addition: "
+                + yourCalculator.addition(5, 3, 7, 8, 1, 9));
+
+        System.out.println();
+
+        System.out.println("===== Default Constructor =====");
+        defaultCalculator.showInfo();
+
+        System.out.println();
+
+        System.out.println("===== Copy Constructor =====");
+        copyCalculator.showInfo();
+
+        System.out.println();
+
+        System.out.println("===== Getter =====");
+        System.out.println("Num1 = " + myCalculator.getNum1());
+
+        System.out.println();
+
+        System.out.println("===== Setter =====");
+        myCalculator.setNum1(100);
+        myCalculator.setNum2(50);
+        myCalculator.showInfo();
+
+        System.out.println();
+
+        System.out.println("===== toString() =====");
+        System.out.println(myCalculator);
+
+        System.out.println();
+
+        System.out.println("Total Objects Created: "
+                + Calculator.getObjectCount());
     }
 }
